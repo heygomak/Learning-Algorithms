9a8c5e54ce6b9865601da849f88aec7b8e8e7d25
heygomak <didvud0323@gmail.com>
2020-11-19 21:52:05 +0900

    패키지 재구성 및 DP 소스 백업

--- Programmers/src/dynamic_programming/Day2_PocketmonTrainner_1.java
+++ Programmers/src/dynamic_programming/Day2_PocketmonTrainner_1.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Day2_PocketmonTrainner {
+public class Day2_PocketmonTrainner_1 {
 
 	static int T; // test_case 개수
 	static int N;
@@ -15,30 +15,14 @@ public class Day2_PocketmonTrainner {
 	static int P[];
 	static List<Integer> visited;
 	static int ans;
-
-	void rotate(int target) {
-		if (visited.contains(target)) {
-			Count++;
-			if (ans < visited.size()) {
-				ans = visited.size();
-			}
-			for (int i = 0; i < visited.size(); i++) {
-				P[visited.get(i)] = -1;
-			}
-			visited = new ArrayList<>();
-		} else {
-			visited.add(visited.size(), target);
-			int temp = P[target];
-			rotate(temp);
-		}
-	}
-
+	
 	public static void main(String[] args) throws Exception {
+		long startTime = System.currentTimeMillis();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
 		T = Integer.parseInt(br.readLine().trim());
-		Day2_PocketmonTrainner pt = new Day2_PocketmonTrainner();
+		Day2_PocketmonTrainner_1 pt = new Day2_PocketmonTrainner_1();
 
 		for (int test_case = 1; test_case < T + 1; test_case++) {
 			N = Integer.parseInt(br.readLine().trim());
@@ -62,7 +46,29 @@ public class Day2_PocketmonTrainner {
 			bw.write("#" + test_case + " " + Count + " " + ans);
 			bw.write("\n");
 		}
+		long endTime = System.currentTimeMillis();
+		long lTime = endTime-startTime;
+		System.out.println("실행시간="+lTime+"(ms)");
+		
 		bw.flush();
 		bw.close();
+	}
+	
+	void rotate(int target) {
+		//System.out.println("visited : "+visited.toString() + " target: " +  target);
+		if (visited.contains(target)) {
+			Count++;
+			if (ans < visited.size()) {
+				ans = visited.size();
+			}
+			for (int i = 0; i < visited.size(); i++) {
+				P[visited.get(i)] = -1;
+			}
+			visited = new ArrayList<>();
+		} else {
+			visited.add(visited.size(), target);
+			int temp = P[target];
+			rotate(temp);
+		}
 	}
 }
---
+++ Programmers/src/dynamic_programming/Day4_VictoryRacer.java
@@ -0,0 +1,7 @@
+package dynamic_programming;
+
+public class Day4_VictoryRacer {
+	public static void main(String[] args) {
+		
+	}
+}
